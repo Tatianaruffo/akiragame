cbd4ce0260df62f263e5e04acddeb3fd50f2fb9c
Tatianaruffo <tatiana.ruffo@gmail.com>
2023-04-12 18:59:25 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome_logo():
+    """
+    Display ASCII game logo on the terminal.
+    """
+    print('Presenting:')
+    print(r"""
+        
+ ______ _              __  ___            __    _          
+/_  __/(_)__ _  ___   /  |/  /___ _ ____ / /   (_)___  ___ 
+ / /  / //  ' \/ -_) / /|_/ // _ `// __// _ \ / // _ \/ -_)
+/_/  /_//_/_/_/\__/ /_/  /_/ \_,_/ \__//_//_//_//_//_/\__/ 
+                                                           
+        """)
+
+def game_rules():
+    """
+    Display the explanation of how the game works 
+    """
+    print("Press any key to start\n")
+    print("The computer will randomly choose a year in the past")
+    print("You'll be given a question about an important fact of the year you're at")
+    print("Type you're answer and you will travel to a new year destination")
+    print("For every right question you'll earn 10 points")
+    print("Guess it right and you'll be able to go back to 2023, otherwise I wish you good luck")
+
+
+
+print(welcome_logo())
+print(game_rules())

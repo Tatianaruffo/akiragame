3833625cdec6a53207187b9948844044b9deaa3b
Tatianaruffo <tatiana.ruffo@gmail.com>
2023-04-13 11:39:37 +0000

    Adds function to explain the game rules

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 def welcome_logo():
     """
-    Display ASCII game logo on the terminal.
+    Display ASCII game logo on the terminal,
+    Display the explanation of how the game works
     """
     print('Presenting:')
     print(r"""
@@ -11,19 +12,45 @@ def welcome_logo():
 /_/  /_//_/_/_/\__/ /_/  /_/ \_,_/ \__//_//_//_//_//_/\__/ 
                                                            
         """)
-
-def game_rules():
-    """
-    Display the explanation of how the game works 
-    """
-    print("Press any key to start\n")
     print("The computer will randomly choose a year in the past")
     print("You'll be given a question about an important fact of the year you're at")
     print("Type you're answer and you will travel to a new year destination")
     print("For every right question you'll earn 10 points")
     print("Guess it right and you'll be able to go back to 2023, otherwise I wish you good luck")
 
+def start_game():
+    """
+    Asks for the username and start the game 
+    """
+    username = " "
+    while True:
+        username = input("Please enter your Name: \n")
 
+        if username.isalnum() != True:
+            print("Error: Letters and numbers only.")
+            continue
+        else:
+            print(f"Hi {username}!")
+            input("When you are ready to time travel, press the Enter key")
+            return username
 
-print(welcome_logo())
-print(game_rules())
+    print(f"Hi {username}!")
+    input("When you're ready to time travel, press the Enter key")
+
+def choose_year():
+    """
+    Loop through the list of years to randomly choose a year
+    """
+    years = [1859, 1920, 1939, 1961, 1968, 1983, 2001]
+    for year in years:
+        return year
+    chosen_year = year
+    return chosen_year
+
+    
+def main():
+    welcome_logo()
+    start_game()
+    choose_year()
+
+main()

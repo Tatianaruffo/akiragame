cc6ea40a7cf8257f217ba30c756da5062d993117
Tatianaruffo <tatiana.ruffo@gmail.com>
2023-04-18 11:02:27 +0000

    Changes the way the code loops through the questions

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random 
+from questions import QUESTIONS
 
 score = 0
 
@@ -41,81 +42,34 @@ def start_game():
     print(f"Hi {username}!")
     input("When you're ready to time travel, press the Enter key")
 
-def choose_year():
-    """
-    Loop through the list of years to randomly choose a year
-    """
-    years = [1859, 1920, 1939, 1961, 1983, 2001]
-    chosen_year = random.choice(years)
 
-    return chosen_year
+def game():
+    for key in QUESTIONS:
+        print(key)
+    answer_input = answer()
+
+    if answer() == QUESTIONS.values():
+        print("Spot on!")
+    else:
+        print("Incorrect!")
 
 
-def show_question(chosen_year):
+def answer():
     """
-    Show the question about the respective year 
+    Get the answer input from user
     """
-    if chosen_year == 1859:
-        print("In 1859 the 'Origin of Species' was published, what's the name of it's author?")
-        ans = input("Write your answer here: \n")
-        if ans == "Charles Darwin":
-            score += 10
-            print("Spot on!")
-        else:
-            print("Incorrect!")
-    elif chosen_year == 1920:
-        print("In 1920 the 19th amendment granted women the right to vote in the USA.")
-        print("What was the name of the movement that fought for this right?")
-        ans = input("Write your answer here: \n")
-        if ans == "suffrage" or "suffragists":
-            score += 10
-            print("Spot on!")
-        else:
-            print("Incorrect!")
-    elif chosen_year == 1939:
-        print("1st of September of 1939 was the beggining of World War II")
-        print("Danzing Harbor was the first place attacked by the Germans")
-        print("In which country is Danzing Harbor located?")
-        ans = input("Write your answer here: \n")
-        if ans == "Poland":
-            score += 10
-            print("Spot on!")
-        else:
-            print("Incorrect!")
-    elif chosen_year == 1961:
-        print("In 1961 the Berlin Wall was built")
-        print("The wall would serve as an enduring symbol of which war?")
-        ans = input("Write your answer here: \n")
-        if ans == "cold war":
-            score += 10
-            print("Spot on!")
-        else:
-            print("Incorrect!")
-    elif chosen_year == 1983:
-        print("At the beginning of 1983, the Internet was born.")
-        print("The Advanced Research Projects Agency Network (ARPANET), transitions to the standard TCP/IP protocol of the WWW, what WWW stands for?")
-        ans = input("Write your answer here: \n")
-        if ans == "World Wide Web":
-            score += 10
-            print("Spot on!")
-        else:
-            print("Incorrect!")
-    elif chosen_year == 2001:
-        print("On the 11th of September, 2001 hijackers inspired by Islamist extremism")
-        print("killed nearly 3,000 people after crashing commercial aircrafts into a famous Manhattan building")
-        print("What was the name of this building?")
-        ans = input("Write your answer here: \n")
-        if ans == "World Trade Center":
-            score += 10
-            print("Spot on!")
-        else:
-            print("Incorrect!")
     
+    answer = input("Write your answer here:\n")
+    answer_input = answer
+
+    return answer_input
+
 
 def main():
     welcome_logo()
     start_game()
-    chosen_year = choose_year()
-    show_question(chosen_year)
+    game()
+    answer()
+
 
 main()

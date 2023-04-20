5a8bcad38293b1f860731594f108984867b3555f
Tatianaruffo <tatiana.ruffo@gmail.com>
2023-04-20 12:10:22 +0000

    Fix questions syntax

--- questions.py
+++ questions.py
@@ -2,40 +2,45 @@
 Questions for the Quiz
 """
 
-q1 = """In 1859 the 'Origin of Species' was published, what's the name of it's author?
-    a.Charles Darwin
-    b.Edgar Allan Poe
-    c.Henry David Thoreau"""
+q1 = """In 1859 the 'Origin of Species' was published, what's the name
+of it's author?
+a.Charles Darwin
+b.Edgar Allan Poe
+c.Henry David Thoreau"""
 
-q2 = """In 1920 the 19th amendment granted women the right to vote in the USA. What was the name of the movement that fought for this right?
-    a.Fauvism
-    b.Suffrage
-    c.Righ to Life"""
+q2 = """In 1920 the 19th amendment granted women the right to vote in the USA.
+What was the name of the movement that fought for this right?
+a.Fauvism
+b.Suffrage
+c.Righ to Life"""
 
-q3 = """1st of September of 1939 was the beggining of World War II, Danzing Harbor was the first place attacked by the Germans, In which country is Danzing Harbor located?
-    a.Poland
-    b.Slovakia
-    c.Croatia"""
+q3 = """1st of September of 1939 was the beggining of World War II,
+Danzing Harbor was the first place attacked by the Germans, In which country
+is Danzing Harbor located?
+a.Poland
+b.Slovakia
+c.Croatia"""
 
-q4 = """In 1961 the Berlin Wall was built, the wall would serve as an enduring symbol of which war?
-    a.World War II
-    b.Cold War
-    c.Operation Juniper Shield"""
+q4 = """In 1961 the Berlin Wall was built, the wall would serve as an
+enduring symbol of which war?
+a.World War II
+b.Cold War
+c.Operation Juniper Shield"""
 
-q5 = """At the beginning of 1983, the Internet was born. The Advanced Research Projects Agency Network (ARPANET), transitions to the standard TCP/IP protocol of the WWW, what WWW stands for?
-    a.Wold Web Wide
-    b.Web Wold Wide
-    c.World Wide Web
-    """
+q5 = """At the beginning of 1983, the Internet was born. The Advanced
+Research Projects Agency Network (ARPANET), transitions to the standard TCP/IP
+protocol of the WWW, what WWW stands for?
+a.Wold Web Wide
+b.Web Wold Wide
+c.World Wide Web
+"""
 
-q6 = """On the 11th of September, 2001 hijackers inspired by Islamist extremism killed nearly 3,000 people after crashing commercial aircrafts into a famous Manhattan building, What was the name of this building?
-    a.Space Needle
-    b.Wold Trade Center
-    c.Fisher Building
-    """
+q6 = """On the 11th of September, 2001 hijackers inspired by Islamist
+extremism killed nearly 3,000 people after crashing commercial aircrafts into
+a famous Manhattan building, What was the name of this building?
+a.Space Needle
+b.Wold Trade Center
+c.Fisher Building
+"""
 
 questions = {q1: "a", q2: "b", q3: "a", q4: "b", q5: "c", q6: "b"}
-
-    
-    
-
